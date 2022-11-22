323ecd025ebe3601f146297c0d7c378518335bf6
kirilltsybrov <kzybrow@yandex.ru>
2022-11-22 20:15:03 +0300

    moved token to a file reworked parse columns function

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from googleapiclient.errors import HttpError
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1wj3WwtE86xgfeJSAIOVXx-cFHX6TAveR4nUsE1Zw170'
-SAMPLE_RANGE_NAME = 'A2:E17'
+
 creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -46,7 +46,12 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKe
 
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters,\
     CallbackQueryHandler, ConversationHandler, Updater
-TOKEN='5702376403:AAHogV1axfPD6WncGqazg9cbF_EXf7D2IIQ'
+
+
+TOKEN=None
+with open('telegram_token.txt', 'r') as f:
+    TOKEN=f.read()
+
 DEMO_TOKEN='5629385379:AAFOTrjhLdi_YjyphxKX871SRT-O4qrvUwk'
 
 UNAUTHORIZED, MENU, PASSPORT, TEAM, TASKS, CONTACTS, PROJECT, CHATS, RESET, CAMERAS, PHOTO_REPORT, SKETCHES, CHART = range(13)
@@ -69,12 +74,7 @@ MENU_BUTTON=[
         [InlineKeyboardButton("Главное меню", callback_data=MENU)],
 ]
 
-def parse_columns(columns):
-    columns_raw = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                  range=columns).execute()
-    columns_raw = columns_raw.get('values', [])
-    baked_columns = [columns_raw[i][0] for i in range(len(columns_raw))]
-    return baked_columns
+
 
 def get_team():
     team_json={}
@@ -82,22 +82,22 @@ def get_team():
         team_json = json.load(f)
     return team_json
 
-def parse_team():
-    service = build('sheets', 'v4', credentials=creds)
+def get_column_by_name(head_row, column_name):
+    search_row=[i.replace(' ','') for i in head_row]
+    search_query=column_name.replace(' ','')
+    return search_row.index(search_query)
 
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-    values = result.get('values', [])
-    return values
+def parse_columns(sheet_name,column_name):
+    columns_raw = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                  range=sheet_name).execute()
+    columns_raw = columns_raw.get('values', [])
+    column_id = get_column_by_name(columns_raw[0],column_name)
+    baked_columns = [columns_raw[i][column_id] for i in range(len(columns_raw)) if column_id<len(columns_raw[i])]
+    return baked_columns[1:]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not os.path.exists('team.json'):
-        with open('team.json', 'w') as f:
-            f.write('{}')
-    team= get_team()
-    a=team.values
+    team = get_team()
+
     if not (update.effective_chat.id in team.values()):
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Введите своё имя и фамилию")
         return UNAUTHORIZED
@@ -108,9 +108,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def authorization(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_creds=update.message.text
-
-    team=parse_columns('Команда!D:D')
-    if user_creds in team:
+    team=parse_columns('Команда','ФИО')
+    if user_creds in team[1:]:
         team_json=get_team()
         team_json[user_creds] = update.effective_chat.id
         with open('team.json', 'w') as f:
@@ -131,7 +130,6 @@ async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return MENU
 
 async def passport(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
     reply_markup = InlineKeyboardMarkup(MENU_BUTTON)
     await context.bot.send_message(chat_id=update.effective_chat.id, text="passport",
                                    reply_markup=reply_markup)
@@ -144,7 +142,7 @@ async def tasks(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     query_role=update.callback_query.data
     tasks_raw = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                   range='Задачи!I:O').execute()
+                                   range='Задачи').execute()
     tasks_raw = tasks_raw.get('values', [])
     tasks = [tasks_raw[i] for i in range(1, len(tasks_raw))]
     # for i in tasks:
@@ -166,9 +164,10 @@ async def tasks(update: Update, context: ContextTypes.DEFAULT_TYPE):
     for i in range(len(user_tasks)):
         message=''
         for j in range(len(COLUMNS)):
+            column_id=get_column_by_name(tasks_raw[0],COLUMNS[j])
             message += '<b>' + COLUMNS[j] + '</b>' + ': '
-            if j < len(user_tasks[i]):
-                message += user_tasks[i][j]
+            if column_id<len(user_tasks[i]):
+                message+=user_tasks[i][column_id]
             message += '\n'
         if i==len(user_tasks)-1:
             await context.bot.send_message(chat_id=update.effective_chat.id, text=message, reply_markup=reply_markup,parse_mode='HTML')
@@ -182,10 +181,8 @@ async def tasks(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return MENU
 
 async def team(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    roles_raw = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                  range='Команда!E:E').execute()
-    roles_raw = roles_raw.get('values', [])
-    roles= [*set([roles_raw[i][0] for i in range(1,len(roles_raw))])]
+    roles_raw=parse_columns('Команда','Роль')
+    roles = [*set([i for i in roles_raw])]
 
     keyboard = []
     for i in roles:
@@ -196,12 +193,13 @@ async def team(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return TEAM
 
 async def role_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    COLUMNS=['ID','Имя','Роль','Телефон']
+    COLUMNS=['ID','ФИО','Роль','Телефон']
     query_role=update.callback_query.data
     roles_raw = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                   range='Команда!C:F').execute()
+                                   range='Команда').execute()
     roles_raw = roles_raw.get('values', [])
     roles = [roles_raw[i] for i in range(1, len(roles_raw))]
+
     employees=[]
     for i in roles:
         if query_role in i:
@@ -209,16 +207,16 @@ async def role_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
     for i in range(len(employees)):
         message=''
         for j in range(len(COLUMNS)):
+            column_id = get_column_by_name(roles_raw[0], COLUMNS[j])
             message+='<b>'+COLUMNS[j]+'</b>'+': '
-            if j<len(employees[i]):
-                message+=employees[i][j]
+            if column_id<len(employees[i]):
+                message += employees[i][column_id]
             message+='\n'
         if i==len(employees)-1:
             reply_markup = InlineKeyboardMarkup(MENU_BUTTON)
             await context.bot.send_message(chat_id=update.effective_chat.id,reply_markup=reply_markup,text=message,parse_mode='HTML')
         else:
             await context.bot.send_message(chat_id=update.effective_chat.id,text=message,parse_mode='HTML')
-
 
     return MENU
 
@@ -228,60 +226,36 @@ async def reset(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text='Введите своё имя и фамилию')
     return UNAUTHORIZED
 
-def get_responsibles():
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range='Задачи!J2:Q10').execute()
-    values=result.get('values', [])
-    undone=[]
-    for i in values:
-        if i[7]=='Отправить напоминание':
-            undone.append([i[0],i[4]])
-    with open('team.json') as f:
-        team = json.load(f)
-
-    return undone
-
-
-async def notifications(context: ContextTypes.DEFAULT_TYPE):
-
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range='Задачи!R2').execute()
-    values = result.get('values', [])
-    if (values[0][0]=='готово'):
-        await context.bot.send_message(chat_id=407354040, text='молодец')
-    elif (values[0][0]=='не готово'):
-        await context.bot.send_message(chat_id=407354040, text='работать')
-
-#407354040
-
-async def echo(context: ContextTypes.DEFAULT_TYPE):
-    context.job_queue.run_repeating(notifications,3)
-
 # "\u041f\u0443\u0448\u043e\u0432\u0438\u0447"
 async def job_monitor(context: ContextTypes.DEFAULT_TYPE):
     COLUMNS = ['Статус', 'Описание', 'Дата начала', 'Дедлайн', 'Дата окончания', 'Ответственный']
     team = get_team()
     tasks_raw = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range='Задачи!I:Q').execute()
+                                range='Задачи').execute()
     tasks_raw = tasks_raw.get('values', [])
     tasks = [tasks_raw[i] for i in range(1, len(tasks_raw))]
-
-
     for i in tasks:
         members=[]
-        if (i[8]=='Отправить напоминание'):
-            members=i[5].split('\n')
+
+        notification_column=get_column_by_name(tasks_raw[0],'Пора отправить напоминание')
+        if (len(i)>notification_column)and(i[notification_column]=='Отправить напоминание'):
+            members_column=get_column_by_name(tasks_raw[0],'Ответственный')
+            members=i[members_column].split('\n')
         for j in members:
             if j in team.keys():
                 message = '<b>ВНИМАНИЕ! Истекает дедлайн задачи!</b>\n'
                 for k in range(len(COLUMNS)):
+                    column_id=get_column_by_name(tasks_raw[0],COLUMNS[k])
                     message += '<b>' + COLUMNS[k] + '</b>' + ': '
-                    if k < len(i):
-                        message +=i[k]
+                    if column_id < len(i):
+                        message +=i[column_id]
                     message += '\n'
                 await context.bot.send_message(chat_id=team[j], text=message,parse_mode='HTML')
 
 if __name__ == '__main__':
+    if not os.path.exists('team.json'):
+        with open('team.json', 'w') as f:
+            f.write('{}')
     application = ApplicationBuilder().token(TOKEN).build()
     updater = application.updater
     queue=application.job_queue
